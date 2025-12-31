afda160315c1117e056a2fdbbebb40a7139b952d
ajeyajaz <ajeyajaz@gmail.com>
2025-12-31 18:00:56 +0530

    Define the UpdateCart action

--- src/redux/features/CartSlice.js
+++ src/redux/features/CartSlice.js
@@ -10,13 +10,31 @@ const cartSlice = createSlice({
     initialState,
     reducers: {
         addCart: (state, action) => {
-            const product = state.cartItems.find(product => product.id === action.payload.id);
-            if (!product) state.cartItems.push({ ...action.payload, quantity: + 1 });
+            const payload = action.payload;
+            const product = state.cartItems.find(product => product.id === payload.id);
+            if (!product) state.cartItems.push(
+                {
+                    ...payload,
+                    quantity: 1,
+                    itemPrice: action.payload.price  // price * quantity
+                });
+        },
+        updateCart: (state, action) => {
+            const payload = action.payload;
+            const product = state.cartItems.find(product => product.id === payload.id);
+
+            const calculatedPrice = product.price * payload.quantity;
+            const originalPrice = calculatedPrice / (1 - product.discountPercentage / 100)
+
+            product.quantity = payload.quantity;
+            product.itemPrice = calculatedPrice.toFixed(2);
+            product.originalPrice = originalPrice.toFixed(2);
+
         }
     }
 });
 
-export const { addCart } = cartSlice.actions;
+export const { addCart, updateCart } = cartSlice.actions;
 export default cartSlice.reducer;
 
 
