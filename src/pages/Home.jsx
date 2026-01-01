2fe1efa884c883b370e16d4fcbd1ced0523429bc
ajeyajaz <ajeyajaz@gmail.com>
2026-01-01 20:27:00 +0530

    define the home Page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import Header from '../components/Header'
+import ProductList from '../components/ProductList'
+import useFetch from '../hooks/useFetch';
+import { useDispatch } from 'react-redux';
+import { addCart } from '../redux/features/CartSlice';
+
+function Home() {
+
+    const { data, error, loader } = useFetch('https://dummyjson.com/products');
+    const dispatch = useDispatch();
+
+    const handleAddCart = (product) => {
+        const cartItem = {
+            id: product.id,
+            brand: product.brand || null,
+            title: product.title,
+            thumbnail: product.thumbnail,
+            price: product.price,
+            discountPercentage: product.discountPercentage,
+        };
+        dispatch(addCart(cartItem))
+    }
+
+
+    if (loader) {
+        return <p>fetching...</p>
+    }
+
+    if (error) {
+        return <p>something went wrong..</p>
+    }
+
+    return (
+        <div>
+            <Header />
+            <ProductList
+                products={data?.products} 
+                onClick={handleAddCart} />
+        </div>
+    )
+}
+
+export default Home
