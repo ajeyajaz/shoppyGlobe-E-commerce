bb053cd7b2b668935df7d67bc7f9f6c4581114c0
ajeyajaz <ajeyajaz@gmail.com>
2026-01-05 08:52:45 +0530

    define the ItemNotFoun Component

---
+++ src/components/ItemNotFound.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router";
+
+
+function ItemNotFound({
+    title = "Item not found",
+    message = "We couldn’t find any items matching your search.",
+    fallBackText = '',
+    url = '/',
+}) {
+    return (
+        <div className="flex flex-col items-center justify-center py-20 text-center">
+
+            <h2 className="text-xl font-semibold text-gray-800">
+                {title}
+            </h2>
+
+            <p className="text-gray-500 mt-2 max-w-sm">
+                {message}
+            </p>
+            {fallBackText && (
+                <Link
+                    to={url}
+                    className="mt-4 text-blue-600 hover:underline font-medium"
+                >
+                    {fallBackText}
+                </Link>
+            )}
+        </div>
+    );
+}
+
+export default ItemNotFound;
