cdce2bad47a57f52bf47186c72f2ac7f9b27e8eb
ajeyajaz <ajeyajaz@gmail.com>
2026-01-05 20:33:35 +0530

    refactore the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,15 @@ import SearchBox from './SearchBox'
 import QuatityBadge from "./QuantityBadge"
 import { HeartIcon, UserIcon, ShoppingBagIcon } from '@heroicons/react/24/outline'
 import { useSelector, useDispatch } from "react-redux"
-import { Link, useNavigate } from "react-router"
+import { Link, useNavigate, useLocation, useMatch, matchPath } from "react-router"
 import { setSearchQuery } from '../redux/features/searchQuerySlice'
 
 function Header() {
 
     const navigate = useNavigate();
+    const { pathname } = useLocation();
+    const isHome = pathname === '/';
+
 
     const cartItemCount = useSelector(state => state.cart.cartItems.length);
     const search = useSelector(state => state.search.query);
@@ -27,10 +30,12 @@ function Header() {
         dispatch(setSearchQuery(query));
     };
 
+   const flex = isHome ? 'flex items-center justify-around' : 'flex items-center justify-between';
+
     return (
         <header
-            className="fixed top-0 left-0 right-0 z-50 flex items-center 
-            justify-around h-20 gap-1 px-2 bg-white flex-wrap shadow-md sm:gap-6">
+            className={`${flex} fixed top-0 left-0 right-0 z-50
+            h-20 gap-1 px-2 bg-white flex-wrap shadow-md sm:gap-6 `}>
 
             {/* Brand logo */}
             <Link to='/'>
@@ -41,52 +46,58 @@ function Header() {
             </Link>
 
             {/* navigation */}
-            <NavBar
-                links={links}
-                className="hidden gap-8 lg:flex"
-                itemClassName="uppercase text-sm font-semibold text-gray-700 hover:text-gray-500  transition-colors">
-            </NavBar>
+            {
+                isHome && (
+                    <NavBar
+                        links={links}
+                        className="hidden gap-8 lg:flex"
+                        itemClassName="uppercase text-sm font-semibold text-gray-700 hover:text-gray-500  transition-colors">
+                    </NavBar>
+                )
+            }
 
 
             {/* searchBox */}
-            <SearchBox
-                placeholder='search products...'
-                onChange={handleSearch}
-                value={search}
-            />
+            {isHome && (
+                <SearchBox
+                    placeholder='search products...'
+                    onChange={handleSearch}
+                    value={search}
+                />
+            )}
 
-            {/* User */}
-            <IconLabel
-                icon={UserIcon}
-                label='User'
-                className="icon-container opacity-effect ml-3"
-                iconClassName="icon-label__icon"
-                labelClassName="icon-label__text"
-            />
-
-            {/* Whishlist*/}
-            <IconLabel
-                icon={HeartIcon}
-                label='Wishlist'
-                className="icon-container opacity-effect"
-                iconClassName="icon-label__icon"
-                labelClassName="icon-label__text"
-            />
-
-            {/* Cart*/}
-            <IconLabel
-                icon={ShoppingBagIcon}
-                onClick={() => navigate('/cart')}
-                label='Bag'
-                className="icon-container opacity-effect relative"
-                iconClassName="icon-label__icon"
-                labelClassName="icon-label__text"
-            >
-                {/* quantity count-badge */}
-                <QuatityBadge
-                    quantity={cartItemCount}
-                    className="absolute -top-2" />
-            </IconLabel>
+            <div className="flex gap-4">
+                {/* User */}
+                <IconLabel
+                    icon={UserIcon}
+                    label='User'
+                    className="icon-container opacity-effect ml-3"
+                    iconClassName="icon-label__icon"
+                    labelClassName="icon-label__text"
+                />
+                {/* Whishlist*/}
+                <IconLabel
+                    icon={HeartIcon}
+                    label='Wishlist'
+                    className="icon-container opacity-effect"
+                    iconClassName="icon-label__icon"
+                    labelClassName="icon-label__text"
+                />
+                {/* Cart*/}
+                <IconLabel
+                    icon={ShoppingBagIcon}
+                    onClick={() => navigate('/cart')}
+                    label='Bag'
+                    className="icon-container opacity-effect relative"
+                    iconClassName="icon-label__icon"
+                    labelClassName="icon-label__text"
+                >
+                    {/* quantity count-badge */}
+                    <QuatityBadge
+                        quantity={cartItemCount}
+                        className="absolute -top-2" />
+                </IconLabel>
+            </div>
         </header>
     )
 }
