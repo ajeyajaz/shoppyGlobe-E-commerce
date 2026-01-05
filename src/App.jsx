68f7988979716a0a3ac48570fd95beedabda09b3
ajeyajaz <ajeyajaz@gmail.com>
2026-01-05 20:34:02 +0530

    refactore the routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,45 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import { lazy, Suspense } from "react"
 import Loader from "./components/Loader";
 import NotFoundPage from './pages/NotFound'
 
+//lazy loaded pages
 const HomePage = lazy(() => import('./pages/Home'));
 const ProductDetailPage = lazy(() => import('./pages/Detail'));
 const CartPage = lazy(() => import('./pages/Cart'));
 const CheckOutPage = lazy(() => import('./pages/Checkout'));
 const OrderPlacedPage = lazy(() => import('./pages/OrderPlaced'));
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/products/:id",
+    element: <ProductDetailPage />,
+  },
+  {
+    path: "/cart",
+    element: <CartPage />,
+  },
+  {
+    path: "/checkout",
+    element: <CheckOutPage />,
+  },
+  {
+    path: "/order-placed",
+    element: <OrderPlacedPage />,
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  }
+])
 
 function App() {
 
@@ -16,32 +47,9 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/'
-            element={<Suspense fallback={<Loader />}><HomePage /></Suspense>}
-          />
-          <Route
-            path='/products/:id'
-            element={<Suspense fallback={<Loader />}><ProductDetailPage /></Suspense>}
-          />
-          <Route
-            path="/cart"
-            element={<Suspense fallback={<Loader />}><CartPage /></Suspense>}
-
-          />
-          <Route path="/checkout"
-            element={<Suspense fallback={<Loader />}><CheckOutPage /></Suspense>}
-          />
-          <Route
-            path="/order-placed"
-            element={<Suspense fallback={<Loader />}><OrderPlacedPage /></Suspense>}
-          />
-          <Route path="*"
-            element={<NotFoundPage />}
-          />
-        </Routes>
-      </BrowserRouter>
+      <Suspense fallback={<Loader/>}>
+        <RouterProvider router={router}/>
+      </Suspense>
     </>
   )
 }
